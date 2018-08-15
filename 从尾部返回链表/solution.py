5e1ab26f21957c5ca11c22334dd45de6d5d9aa7d
markson14 <markson14zhang@gmail.com>
2018-08-15 20:43:45 +1000

    8.15 practices

---
+++ 从尾部返回链表/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    # 返回从尾部到头部的列表值序列，例如[1,2,3]
+    def printListFromTailToHead(self, listNode):
+        # write code here
+        if listNode is None:
+            return []
+        return self.printListFromTailToHead(listNode.next) + [listNode.val]
