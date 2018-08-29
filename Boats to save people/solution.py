0bead2e0ad7198c1df1ba8123f41a360e065fb6b
markson14 <markson14zhang@gmail.com>
2018-08-30 00:01:09 +1000

    greedy

---
+++ Boats to save people/solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def numRescueBoats(self, people, limit):
+        """
+        :type people: List[int]
+        :type limit: int
+        :rtype: int
+        """
+        people = sorted(people)
+        m,n = 0,len(people)
+        for i in range(n-1,-1,-1):
+            if people[m] + people[i] <=limit:
+                m+=1
+            if m>=i:
+                return n-i
+                
