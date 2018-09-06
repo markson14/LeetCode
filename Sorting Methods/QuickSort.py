2066153bb9a3bae4c3ce31e31a142242984897b3
markson14 <markson14zhang@gmail.com>
2018-09-06 10:43:21 +1000

    Sorting Methods

---
+++ Sorting Methods/QuickSort.py
@@ -0,0 +1,47 @@
+"""
+    快速排序：
+        1.首先L0为基准值
+        2.小于pivot的移项pivot左边，大于pivot的移项pivot右侧。
+        3.然后在对pivot左右两个区间
+        4.左右区间重复1,2,3知道区间长度等于1
+"""
+def quickSort(alist):
+    quickSortHelper(alist,0,len(alist)-1)
+
+def quickSortHelper(alist,first,last):
+    if first<last:
+
+        splitpoint = partition(alist,first,last)
+
+        quickSortHelper(alist,first,splitpoint-1)
+        quickSortHelper(alist,splitpoint+1,last)
+
+
+def partition(alist,first,last):
+    pivotvalue = alist[first]
+
+    leftmark = first+1
+    rightmark = last
+
+    done = False
+    while not done:
+
+        while leftmark <= rightmark and alist[leftmark] <= pivotvalue:
+            leftmark = leftmark + 1
+
+        while alist[rightmark] >= pivotvalue and rightmark >= leftmark:
+            rightmark = rightmark -1
+
+        if rightmark < leftmark:
+            done = True
+        else:
+            temp = alist[leftmark]
+            alist[leftmark] = alist[rightmark]
+            alist[rightmark] = temp
+
+    temp = alist[first]
+    alist[first] = alist[rightmark]
+    alist[rightmark] = temp
+
+
+    return rightmark
