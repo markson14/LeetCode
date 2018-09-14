34503fc16ef385f14fb741bdc1b9168d2a7c88b6
markson14 <markson14zhang@gmail.com>
2018-09-14 18:40:24 +1000

    Update solution.py

--- Solution/Longest Increasing SubString/solution.py
+++ Solution/Longest Increasing SubString/solution.py
@@ -12,21 +12,20 @@ class Solution(object): # 1384ms
                     res[i] = max(res[i],res[j]+1)
         return max(res)
 
-class Solution(object): # 32ms
-    def lengthOfLIS(self, nums):
+class Solution:
+    """
+        time: n
+        space n
+    """
+    def findLengthOfLCIS(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        if not nums or len(nums) == 0:
-            return 0
-        
-        lis = [nums[0]]
-        for i in range(1, len(nums)):
-            if nums[i] > lis[-1]:
-                lis.append(nums[i])
-            else:
-                lis[bisect.bisect_left(lis, nums[i])] = nums[i]
-        
-        return len(lis)
+        if not nums: return 0
+        res = [1] * len(nums)
+        for i in range(1,len(nums)):
+            if nums[i]-nums[i-1] > 0:
+                res[i] = res[i-1]+1
+        return max(res)
             
