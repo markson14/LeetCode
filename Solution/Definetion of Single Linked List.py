8486937ae40602c08b115329a617410cf8cd0aec
markson14 <markson14zhang@gmail.com>
2018-10-10 19:24:55 +1100

    linkedlist

--- Solution/Definetion of Single Linked List.py
+++ Solution/Definetion of Single Linked List.py
@@ -2,15 +2,37 @@ class Node:
     def __init__(self, val):
         self.val = val
         self.next = None
-node1 = Node(32)
-node2 = Node(83)
-node3 = Node(11)
+
+# Set up LinkedList
+node1 = Node(11)
+node2 = Node(22)
+node3 = Node(33)
 node1.next = node2
 node2.next = node3
 
-head = node1
+head = cur = top = node1
 out = []
+res = []
+
+# print input linkedlist
 while head:
     out.append(head.val)
+    print(bool(head.next))
     head = head.next
-print(out)
+    
+
+# Delete Node
+while cur:
+    if cur.val == 22:
+        cur.val = cur.next.val
+        cur.next = cur.next.next
+    else:
+        cur = cur.next
+
+# print output linkedlist
+while top:
+    res.append(top.val)
+    top = top.next
+
+print("input: {}".format(out))
+print("output: {}".format(res))
