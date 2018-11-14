76a934441022ad03486473a3f6d19676947b7fa6
markson14 <markson14zhang@gmail.com>
2018-11-14 23:27:58 +1100

    11.14 linkednode

--- Solution/Definetion of Single Linked List.py
+++ Solution/Definetion of Single Linked List.py
@@ -2,6 +2,7 @@ class Node:
     def __init__(self, val):
         self.val = val
         self.next = None
+
     def __repr__(self) -> str:
         return '{} -> {} '.format(self.val, repr(self.next))
     def append(self, val):
@@ -9,6 +10,37 @@ class Node:
         while cur.next is not None:
             cur = cur.next
         cur.next = Node(val)
+    def delete(self, val):
+        cur = self
+        while cur:
+            if cur.val == val:
+                cur.val = cur.next.val
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
+    def insert(self,index,val):
+        cur = self
+        nxt = self.next
+        counter = 1
+        if counter == index:
+            tmp = Node(val)
+            cur.next = tmp
+            tmp.next = nxt
+        else:
+            counter += 1
+            cur = nxt
+            nxt = nxt.next
+
+def reversed(root):
+    pre = None
+    cur = root
+    while cur:
+        post = cur.next
+        cur.next = pre
+        pre = cur
+        cur = post
+    root = pre
+    return root
 
 def list_append_listnode(lst):
     head = Node(0)
@@ -16,6 +48,7 @@ def list_append_listnode(lst):
         head.append(i)
     return head.next
 
+################################################
 # Set up LinkedList
 lst = [11,22,33]
 
@@ -23,14 +56,18 @@ head = cur = top = list_append_listnode(lst)
 print()
 print("input: {}".format(head))
 # Delete Node
-while cur:
-    if cur.val == 22:
-        cur.val = cur.next.val
-        cur.next = cur.next.next
-    else:
-        cur = cur.next
 
-# print output linkedlist
+head.delete(22)
+print("delete node: {}".format(head))
+# Append Node at the end
 
-print("output: {}".format(head))
+head.append(22)
+print("Append Node: {}".format(head))
+# Insert Node
+
+head.insert(1,22)
+print("Insert Node: {}".format(head))
+# reverse Node
+head = reverse(head)
+print("Reverse Node: {}".format(head))
 print()
