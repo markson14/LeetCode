660b7536f6d73a5016cf23abf2afe72037b28c72
markson14 <markson14zhang@gmail.com>
2018-12-07 12:48:53 +1100

    12.7 new methods

--- Solution/4Sum/solution.py
+++ Solution/4Sum/solution.py
@@ -6,7 +6,7 @@ class Solution:
         :rtype: List[List[int]]
         """
         def findNsum(l,r,target,N,result,res):
-            if r-l+1 < N or N<2 or target < nums[l]*N or target > nums[r]*N:  # early termination
+            if r-l+1 < N or N<2 or target < nums[l]*N or target > nums[r]*N:  # early termination which is the key point to reduce time complexity
                 return
             if N == 2: # two pointers solve sorted 2-sum problem
                 while l < r:
@@ -27,4 +27,50 @@ class Solution:
         nums.sort()
         res = []
         findNsum(0,len(nums)-1,target,4,[],res)
+        return res
+
+
+
+class Solution2:
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        n = len(nums)
+        nums.sort()
+        res = []
+        for i in range(n-3):
+            cur1 = target
+            if i != 0 and nums[i] == nums[i-1]:
+                continue
+            cur2 = cur1 - nums[i]
+            for j in range(i+1,n-2):
+                if j != i+1 and nums[j] == nums[j-1]:
+                    continue
+                cur3 = cur2 - nums[j]
+                left = j+1
+                right = n-1
+                ###############################################
+                
+                """
+                    early termination是缩短时间的关键，从超时到130ms
+                """
+                if cur3 < nums[left]*2 or cur3 > nums[right]*2:
+                    continue
+                ###############################################
+                while left < right:
+                    if nums[left]+nums[right] == cur3:
+                        # print(nums)
+                        # print(i,j,left,right)
+                        res.append([nums[i],nums[j],nums[left],nums[right]])
+                        left += 1
+                        right -= 1
+                        while left < right and nums[left] == nums[left-1]: left += 1
+                        while left < right and nums[right] == nums[right+1]: right -= 1
+                    elif nums[left]+nums[right] > cur3:
+                        right -= 1
+                    else:
+                        left += 1
         return res
--- Solution/Remove Nth Node From End of List/Solution.py
+++ Solution/Remove Nth Node From End of List/Solution.py
@@ -4,7 +4,7 @@ class ListNode:
         self.val = x
         self.next = None
 
-class Solution:
+class Solution: # scan twice, first time to get the length.
     def removeNthFromEnd(self, head, n):
         """
         :type head: ListNode
@@ -25,3 +25,21 @@ class Solution:
             top = top.next
         top.next = top.next.next
         return x.next
+
+class Solution2: # using two pointer which is even fast, only scan once
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        fast = slow = head
+        for i in range(n):
+            fast = fast.next
+        if fast is None:
+            return head.next
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+        return head
