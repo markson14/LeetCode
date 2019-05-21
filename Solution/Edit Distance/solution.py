459277fc06e28e117393270f9efb3bd757366186
markson14 <markson14zhang@gmail.com>
2019-05-21 11:14:08 +0800

    editdistance

--- Solution/Edit Distance/solution.py
+++ Solution/Edit Distance/solution.py
@@ -1,22 +1,17 @@
-class Solution(object):
-    def minDistance(self, word1, word2):
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: int
-        """
-        rows = len(word1) + 1
-        cols = len(word2) + 1
-        dp = [ [ 0 for i in range(cols) ] for i in range(rows) ]
-        dp[0] = [ i for i in range(cols) ]
-        
-        for i in range(rows):
-            dp[i][0] = i
-        
-        for i in range(1, rows):
-            for j in range(1, cols):
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        n = len(word1)+1
+        m = len(word2)+1
+        pad = [[0]*m for _ in range(n)]
+        for i in range(m):
+            pad[0][i] = i
+        for j in range(n):
+            pad[j][0] = j
+        temp = None
+        for i in range(1,n):
+            for j in range(1,m):
                 if word1[i-1] == word2[j-1]:
-                    dp[i][j] = dp[i-1][j-1]
-                else:
-                    dp[i][j] = 1 + min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1])
-        return dp[rows-1][cols-1]
+                    temp = 0
+                else:temp = 1
+                pad[i][j] = min(pad[i-1][j]+1,pad[i][j-1]+1,pad[i-1][j-1]+temp)
+        return pad[-1][-1]
